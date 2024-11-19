7cd1e52c80f3885c688c54039c8c7db157a9d76b
AndersonTsaiTW <AndersonTsaiTW@gmail.com>
2024-11-19 09:49:52 -0500

    update 'https://andersontsaitw.github.io/CF_Ach6_myFlix-Angular-client' to CORS

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const express = require('express'),
 // the modules about cors
 // cors is used to control the login location
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://andersoncfmyfilx.netlify.app', 'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://andersoncfmyfilx.netlify.app', 'http://localhost:4200', 'https://andersontsaitw.github.io/CF_Ach6_myFlix-Angular-client'];
 
 app.use(cors({
   origin: (origin, callback) => {
